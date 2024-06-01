41766b45e6c348b57b4c3d94504d915ac271aee7
Boxhead78 <maik_baumann90@web.de>
2024-06-01 15:12:32 +0200

    Update Module

--- modules/mod-individual-progression/sql/world/base/mounts_and_riding.sql
+++ modules/mod-individual-progression/sql/world/base/mounts_and_riding.sql
@@ -1,10 +1,14 @@
 -- Re-enable Summon Felsteed (Warlock)
 DELETE FROM `disables` WHERE `sourceType`=1 AND `entry` IN (3631, 4487, 4488, 4489, 4490);
 
+-- Move letter about cold weather flying to level 71 instead of 70, as in Wotlk classic
+UPDATE `mail_level_reward` SET `level` = 71 WHERE `mailTemplateId` IN (285, 284);
+
 -- Riding Skills
 UPDATE npc_trainer SET MoneyCost=800000 WHERE SpellID=33388;
 UPDATE npc_trainer SET MoneyCost=10000000 WHERE SpellID=33391;
 UPDATE npc_trainer SET MoneyCost=6000000 WHERE SpellID=34090;
+DELETE FROM npc_trainer WHERE SpellID=13819;
 DELETE FROM npc_trainer WHERE SpellID=23214;
 DELETE FROM npc_trainer WHERE SpellID=34767;
 DELETE FROM npc_trainer WHERE SpellID=23161;
